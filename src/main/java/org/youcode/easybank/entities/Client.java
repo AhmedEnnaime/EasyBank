a1efcbc90261ba217ada89ee2b241b4992f50729
AhmedEnnaime <97989224+AhmedEnnaime@users.noreply.github.com>
2023-09-21 12:02:52 +0100

    made a simple change in Client entity

--- src/main/java/org/youcode/easybank/entities/Client.java
+++ src/main/java/org/youcode/easybank/entities/Client.java
@@ -5,6 +5,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @Data
 @AllArgsConstructor
@@ -12,12 +13,12 @@ import java.time.LocalDate;
 public class Client extends Person{
     private int _code;
 
-    private Account _account;
+    private List<Account> _accounts;
 
-    public Client(String lastName, String firstName, LocalDate birthDate, String phone, String address, int code, Account account) {
+    public Client(String lastName, String firstName, LocalDate birthDate, String phone, String address, int code, List<Account> accounts) {
         super(lastName, firstName, birthDate, phone, address);
         this._code = code;
-        this._account = account;
+        this._accounts = accounts;
     }
 
 }
