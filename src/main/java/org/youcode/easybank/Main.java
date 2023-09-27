453673a3d97c1650d1975fa8d7aac19a8772f486
AhmedEnnaime <97989224+AhmedEnnaime@users.noreply.github.com>
2023-09-27 12:15:49 +0100

    EAS-12 builded the menu in the main and working on missionAssignment service

--- src/main/java/org/youcode/easybank/Main.java
+++ src/main/java/org/youcode/easybank/Main.java
@@ -1,31 +1,329 @@
 package org.youcode.easybank;
 
+import org.youcode.easybank.db.DBConnection;
 import org.youcode.easybank.exceptions.AccountException;
 import org.youcode.easybank.exceptions.ClientException;
 import org.youcode.easybank.exceptions.EmployeeException;
 import org.youcode.easybank.exceptions.OperationException;
 import org.youcode.easybank.services.*;
 
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
 
-//        EmployeeService.createEmployee();
-//        ClientService.createClient();
+            while (true) {
+                System.out.println("Welcome to Your Bank Application!");
+                System.out.println("1. Employee Management");
+                System.out.println("2. Client Management");
+                System.out.println("3. Account Management");
+                System.out.println("4. Operation Management");
+                System.out.println("5. Mission Management");
+                System.out.println("6. Mission Assignment Management");
+                System.out.println("7. Exit");
+                System.out.print("Enter your choice: ");
 
-//        try {
-//            AccountService.createAccount();
-//        }catch (ClientException | EmployeeException a) {
-//            a.printStackTrace();
-//        }
-//            AccountService.updateAccount();
+                int choice = sc.nextInt();
+                sc.nextLine();
 
-//        try {
-//            OperationService.createOperation();
-//        }catch (AccountException | EmployeeException e) {
-//            throw new RuntimeException(e);
-//        }
+                switch (choice) {
+                    case 1:
+                        employeeManagementMenu();
+                        break;
+                    case 2:
+                        clientManagementMenu();
+                        break;
+                    case 3:
+                        accountManagementMenu();
+                        break;
+                    case 4:
+                        operationManagementMenu();
+                        break;
+                    case 5:
+                        missionManagementMenu();
+                        break;
+                    case 6:
+                        missionAssignmentManagementMenu();
+                        break;
+                    case 7:
+                        System.out.println("Exiting the application. Goodbye!");
+                        DBConnection.closeConnection();
+                        sc.close();
+                        System.exit(0);
+                    default:
+                        System.out.println("Invalid choice. Please select a valid option.");
+                        break;
+                }
+            }
+        }
 
-        MissionService.deleteMission();
+        private static void employeeManagementMenu() {
+            Scanner sc = new Scanner(System.in);
 
+            while (true) {
+                System.out.println("Employee Management Menu:");
+                System.out.println("1. Create Employee");
+                System.out.println("2. Update Employee");
+                System.out.println("3. Delete Employee");
+                System.out.println("4. Get Employee by Matricule");
+                System.out.println("5. Get All Employees");
+                System.out.println("6. Find Employee by Attribute");
+                System.out.println("7. Back to Main Menu");
+                System.out.print("Enter your choice: ");
+
+                int choice = sc.nextInt();
+                sc.nextLine();
+
+                switch (choice) {
+                    case 1:
+                        EmployeeService.createEmployee();
+                        break;
+                    case 2:
+                        EmployeeService.updateEmployee();
+                        break;
+                    case 3:
+                        EmployeeService.deleteEmployee();
+                        break;
+                    case 4:
+                        EmployeeService.getEmployeeByMatricule();
+                        break;
+                    case 5:
+                        EmployeeService.getAllEmployees();
+                        break;
+                    case 6:
+                        EmployeeService.findEmployeeByAttribute();
+                        break;
+                    case 7:
+                        return;
+                    default:
+                        System.out.println("Invalid choice. Please select a valid option.");
+                        break;
+                }
+            }
+        }
+
+        private static void clientManagementMenu() {
+            Scanner sc = new Scanner(System.in);
+
+            while (true) {
+                System.out.println("Client Management Menu:");
+                System.out.println("1. Create Client");
+                System.out.println("2. Update Client");
+                System.out.println("3. Delete Client");
+                System.out.println("4. Get Client by Code");
+                System.out.println("5. Get All Clients");
+                System.out.println("6. Find Client by Attribute");
+                System.out.println("7. Back to Main Menu");
+                System.out.print("Enter your choice: ");
+
+                int choice = sc.nextInt();
+                sc.nextLine();
+
+                switch (choice) {
+                    case 1:
+                        ClientService.createClient();
+                        break;
+                    case 2:
+                        ClientService.updateClient();
+                        break;
+                    case 3:
+                        ClientService.deleteClient();
+                        break;
+                    case 4:
+                        ClientService.getClientByCode();
+                        break;
+                    case 5:
+                        ClientService.getAllClients();
+                        break;
+                    case 6:
+                        ClientService.findClientByAttribute();
+                        break;
+                    case 7:
+                        return; // Return to the main menu
+                    default:
+                        System.out.println("Invalid choice. Please select a valid option.");
+                        break;
+                }
+            }
+        }
+
+        private static void accountManagementMenu() {
+            Scanner sc = new Scanner(System.in);
+
+            while (true) {
+                System.out.println("Account Management Menu:");
+                System.out.println("1. Create Account");
+                System.out.println("2. Update Account");
+                System.out.println("3. Get Account by Operation Number");
+                System.out.println("4. Update Account Status");
+                System.out.println("5. Display all savings accounts");
+                System.out.println("6. Display all current accounts");
+                System.out.println("7. Display accounts by their creation date");
+                System.out.println("8. Delete Account");
+                System.out.println("9. Get Accounts By Their Status");
+                System.out.println("10. Get Account By Account Number");
+                System.out.println("11. Get Accounts By Client");
+                System.out.println("12. Back to Main Menu");
+                System.out.print("Enter your choice: ");
+
+                int choice = sc.nextInt();
+                sc.nextLine();
+
+                switch (choice) {
+                    case 1:
+                        try {
+                            AccountService.createAccount();
+                        } catch (ClientException | EmployeeException a) {
+                            a.printStackTrace();
+                        }
+                        break;
+                    case 2:
+                        AccountService.updateAccount();
+                        break;
+                    case 3:
+                        AccountService.getAccountByOperationNumber();
+                        break;
+                    case 4:
+                        AccountService.updateAccountStatus();
+                        break;
+                    case 5:
+                        AccountService.getAllSavingsAccounts();
+                        break;
+                    case 6:
+                        AccountService.getAllCurrentAccounts();
+                        break;
+                    case 7:
+                        AccountService.getAllAccountsByCreationDate();
+                        break;
+                    case 8:
+                        AccountService.deleteAccount();
+                        break;
+                    case 9:
+                        AccountService.getAccountsByStatus();
+                        break;
+                    case 10:
+                        AccountService.getAccountByAccountNumber();
+                        break;
+                    case 11:
+                        try {
+                            AccountService.getAccountsByClient();
+                        } catch (AccountException e) {
+                            throw new RuntimeException(e);
+                        }
+                        break;
+                    case 12:
+                        return;
+                    default:
+                        System.out.println("Invalid choice. Please select a valid option.");
+                        break;
+                }
+            }
+        }
+
+        private static void operationManagementMenu() {
+            Scanner sc = new Scanner(System.in);
+
+            while (true) {
+                System.out.println("Operation Management Menu:");
+                System.out.println("1. Create Operation");
+                System.out.println("2. Get Operation By Number");
+                System.out.println("3. Delete Operation");
+                System.out.println("4. Back to Main Menu");
+                System.out.print("Enter your choice: ");
+
+                int choice = sc.nextInt();
+                sc.nextLine();
+
+                switch (choice) {
+                    case 1:
+                        try {
+                            OperationService.createOperation();
+                        } catch (AccountException | EmployeeException e) {
+                            throw new RuntimeException(e);
+                        }
+                        break;
+                    case 2:
+                        OperationService.getOperationByNumber();
+                        break;
+                    case 3:
+                        OperationService.deleteOperation();
+                        break;
+                    case 4:
+                        return;
+                    default:
+                        System.out.println("Invalid choice. Please select a valid option.");
+                        break;
+                }
+            }
+        }
+
+        private static void missionManagementMenu() {
+            Scanner sc = new Scanner(System.in);
+
+            while (true) {
+                System.out.println("Mission Management Menu:");
+                System.out.println("1. Create Mission");
+                System.out.println("2. Get Mission By Number");
+                System.out.println("3. Delete Mission");
+                System.out.println("4. Back to Main Menu");
+                System.out.print("Enter your choice: ");
+
+                int choice = sc.nextInt();
+                sc.nextLine();
+
+                switch (choice) {
+                    case 1:
+                        MissionService.createMission();
+                        break;
+                    case 2:
+                        MissionService.getMissionByNumber();
+                        break;
+                    case 3:
+                        MissionService.deleteMission();
+                        break;
+                    case 4:
+                        return;
+                    default:
+                        System.out.println("Invalid choice. Please select a valid option.");
+                        break;
+                }
+            }
+        }
+    private static void missionAssignmentManagementMenu() {
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Mission Assignment Management Menu:");
+            System.out.println("1. Assign Mission");
+            System.out.println("2. Get Assignment By ID");
+            System.out.println("3. Delete Mission Assignment");
+            System.out.println("4. Get Employees Assignments");
+            System.out.println("5. Back to Main Menu");
+            System.out.print("Enter your choice: ");
+
+            int choice = sc.nextInt();
+            sc.nextLine();
+
+            switch (choice) {
+                case 1:
+                    MissionAssignmentService.createMissionAssignment();
+                    break;
+                case 2:
+                    MissionAssignmentService.getAssignmentByID();
+                    break;
+                case 3:
+                    MissionAssignmentService.deleteAssignment();
+                    break;
+                case 4:
+                    MissionAssignmentService.getEmployeesAssignments();
+                    break;
+                case 5:
+                    return;
+                default:
+                    System.out.println("Invalid choice. Please select a valid option.");
+                    break;
+            }
+        }
     }
 }
--- src/main/java/org/youcode/easybank/services/MissionAssignmentService.java
+++ src/main/java/org/youcode/easybank/services/MissionAssignmentService.java
@@ -1,8 +1,129 @@
 package org.youcode.easybank.services;
 
+import org.youcode.easybank.dao.EmployeeDao;
+import org.youcode.easybank.dao.MissionAssignmentDao;
+import org.youcode.easybank.dao.daoImpl.EmployeeDaoImpl;
+import org.youcode.easybank.dao.daoImpl.MissionAssignmentDaoImpl;
+import org.youcode.easybank.entities.Employee;
+import org.youcode.easybank.entities.MissionAssignment;
+import org.youcode.easybank.exceptions.EmployeeException;
+import org.youcode.easybank.exceptions.MissionAssignmentException;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.Scanner;
+
 public class MissionAssignmentService {
 
     public static void createMissionAssignment() {
 
     }
+
+    public static void getEmployeesAssignments() {
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Enter the employee's matricule to retrieve mission assignments (or enter 'q' to quit): ");
+            String input = sc.nextLine();
+
+            if (input.equalsIgnoreCase("q")) {
+                break;
+            }
+
+            try {
+                int matricule = Integer.parseInt(input);
+                EmployeeDao employeeDao = new EmployeeDaoImpl();
+                Optional<Employee> employee = employeeDao.getByMatricule(matricule);
+
+                if (employee.isPresent()) {
+                    MissionAssignmentDao missionAssignmentDao = new MissionAssignmentDaoImpl();
+                    List<MissionAssignment> assignments = missionAssignmentDao.getEmployeeAssignments(employee.get());
+
+                    if (!assignments.isEmpty()) {
+                        System.out.println("Mission Assignments for Employee with Matricule " + matricule + ":");
+                        for (MissionAssignment assignment : assignments) {
+                            System.out.println("Assignment ID: " + assignment.getId());
+                            System.out.println("Start Date: " + assignment.get_debutDate());
+                            System.out.println("End Date: " + assignment.get_endDate());
+                            System.out.println("---------------------------");
+                        }
+                    } else {
+                        System.out.println("No mission assignments found for the employee with Matricule " + matricule + ".");
+                    }
+                } else {
+                    System.out.println("Employee not found with Matricule " + matricule + ". Please try again.");
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid input. Please enter a valid matricule or 'q' to quit.");
+            } catch (MissionAssignmentException e) {
+                System.out.println("Error retrieving mission assignments: " + e.getMessage());
+            } catch (EmployeeException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    public static void deleteAssignment() {
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Enter the ID of the mission assignment to delete (or enter 'q' to quit): ");
+            String input = sc.nextLine();
+
+            if (input.equalsIgnoreCase("q")) {
+                break;
+            }
+
+            try {
+                int assignmentId = Integer.parseInt(input);
+                MissionAssignmentDao missionAssignmentDao = new MissionAssignmentDaoImpl();
+
+                if (missionAssignmentDao.delete(assignmentId)) {
+                    System.out.println("Mission assignment with ID " + assignmentId + " deleted successfully.");
+                } else {
+                    System.out.println("Mission assignment with ID " + assignmentId + " not found.");
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid input. Please enter a valid ID or 'q' to quit.");
+            } catch (MissionAssignmentException e) {
+                System.out.println("Error deleting mission assignment: " + e.getMessage());
+            }
+        }
+    }
+
+    public static void getAssignmentByID() {
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Enter the ID of the mission assignment to retrieve (or enter 'q' to quit): ");
+            String input = sc.nextLine();
+
+            if (input.equalsIgnoreCase("q")) {
+                break;
+            }
+
+            try {
+                int assignmentId = Integer.parseInt(input);
+                MissionAssignmentDao missionAssignmentDao = new MissionAssignmentDaoImpl();
+                Optional<MissionAssignment> assignment = missionAssignmentDao.getByID(assignmentId);
+
+                if (assignment.isPresent()) {
+                    MissionAssignment missionAssignment = assignment.get();
+                    System.out.println("Mission Assignment with ID " + assignmentId + ":");
+                    System.out.println("Start Date: " + missionAssignment.get_debutDate());
+                    System.out.println("End Date: " + missionAssignment.get_endDate());
+                    System.out.println("---------------------------");
+                } else {
+                    System.out.println("Mission assignment with ID " + assignmentId + " not found.");
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid input. Please enter a valid ID or 'q' to quit.");
+            } catch (MissionAssignmentException e) {
+                System.out.println("Error retrieving mission assignment: " + e.getMessage());
+            }
+        }
+    }
+
+
+
 }
